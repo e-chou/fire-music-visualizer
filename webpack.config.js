d5b670ef31a09a9a3995bd194b1e10cc1bc6e088
e-chou <nutcasealpha@gmail.com>
2022-09-21 21:57:38 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: '/hw01-fireball-base/',
+    publicPath: '/fire-music-visualizer/',
   },
   module: {
     rules: [
