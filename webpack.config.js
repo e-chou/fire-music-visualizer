c2dcd456f4c60d4e429fc68ebfffe9b364edd703
Elizabeth Chou <elyssa@Elizabeths-MacBook-Pro-4.local>
2022-09-21 20:19:59 -0400

    changed path in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: '/',
+    publicPath: '/hw01-fireball-base/',
   },
   module: {
     rules: [
